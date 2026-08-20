0367dd2493ff6c0e752c1e28b8f942c017f5b31d
divygupta304-coder <divy.gupta.304@gmail.com>
2026-08-20 09:35:35 +0530

    update 2

--- day_5/usefetch.js
+++ day_5/usefetch.js
@@ -3,13 +3,20 @@ const button=document.getElementById('btn')
 // console.log(div);
 const h2=document.createElement('h2');
 h2.innerText='Data is loading....';
-div.appendChild(h2);
 console.log(h2);
 async function display(){
-// div.innerHTML='<h2>Hello Using DOM</h2>';
-const serverdata=await fetch('https://fakestoreapi.com/products');
-            const jsonData= await serverdata.json();
-            console.log(jsonData[1].title)
+    try{
+        // div.innerHTML='<h2>Hello Using DOM</h2>';
+        div.appendChild(h2);
+        const serverdata=await fetch('https://fakestoreapi.com/products');
+        const jsonData= await serverdata.json();
+        // console.log(jsonData[1].title)
+
+        div.innerHTML=`<h2 style="color: blue;">${JSON.stringify(jsonData)}</h2>`;
+    }
+    catch(err){
+        console.log("Error is" + err);
+    }
 
 }
 button.addEventListener('click',display);
